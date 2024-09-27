cb768674ab5bf4049b7acc46c642a268c4dc20b6
Carsten Bach <mail@carsten-bach.de>
2024-09-27 21:48:47 +0200

    Init with needed files

---
+++ readme.md
@@ -0,0 +1,12 @@
+# test-wporg-replace
+
+Stable tag: 0.31.0-alpha  
+Tested up to: 6.6.1  
+License: GPL v2 or later  
+Tags: events, event, meetup, community  
+Contributors: mauteri, hrmervin, patricia70, carstenbach, jmarx75, stephenerdelyi, calebthedev, prayagm, pbrocks, linusx007
+
+
+[![Playground Demo Link](https://img.shields.io/badge/WordPress_Playground-blue?logo=wordpress&logoColor=%23fff&labelColor=%233858e9&color=%233858e9)](https://playground.wordpress.net/?blueprint-url=https://raw.githubusercontent.com/GatherPress/gatherpress/main/.wordpress-org/blueprints/blueprint.json)
+
+...
---
+++ test-wporg-replace-plugin.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name:       test-wporg-replace
+ * Plugin URI:        https://github.com/carstingaxion/test-wporg-replace
+ * Description:       Powering Communities with WordPress.
+ * Version:           0.31.0-alpha
+ * Requires PHP:      7.4
+ * Requires at least: 6.6
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit; // @codeCoverageIgnore
+/* 
+// Constants.
+define( 'GATHERPRESS_CORE_FILE', __FILE__ );
+define( 'GATHERPRESS_CORE_PATH', __DIR__ );
+define( 'GATHERPRESS_CORE_URL', plugin_dir_url( __FILE__ ) );
+define( 'GATHERPRESS_DIR_NAME', dirname( plugin_basename( __FILE__ ) ) );
+define( 'GATHERPRESS_REQUIRES_PHP', current( get_file_data( __FILE__, array( 'Requires PHP' ), 'plugin' ) ) );
+define( 'GATHERPRESS_REST_NAMESPACE', 'gatherpress/v1' );
+define( 'GATHERPRESS_VERSION', current( get_file_data( __FILE__, array( 'Version' ), 'plugin' ) ) );
+ */
